52fc37590d56d38deaf3b87881b653c3fc19ed63
Jason Long <longwunet@gmail.com>
2014-10-16 14:38:22 +0800

    增加【QueryOrderWaitTime】实体

---
+++ TrainAssistant/Models/QueryOrderWaitTime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainAssistant.Models
+{
+    public class QueryOrderWaitTime
+    {
+        public bool Status { get; set; }
+        public int Count { get; set; }
+        public int WaitTime { get; set; }
+        public string RequestId { get; set; }
+        public int WaitCount { get; set; }
+        public string TourFlag { get; set; }
+        public string OrderId { get; set; }
+    }
+}
